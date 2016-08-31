913037a33421da5a1e053f223c004d247e58cdc6
dvychen <dvychen@gmail.com>
2016-08-31 12:20:48 -0400

    gave constants a variable
    ALL_CAPS_WITH_UNDERSCORES

--- ExpCalculator.py
+++ ExpCalculator.py
@@ -12,37 +12,38 @@ def rtnh(rofl):
     else:
         return 404
 
+#Constants
+CANDY_PER_PK = 4
+EXP_FROM_EVOLUTION = 500
 
 print "Welcome to the Pokemon Go Exp Calculator!"
 print "Main Menu"
 print "1. Basic/Got all the candy"
 print "2. Smart/Catching and Evolving one Pokemon"
-choice = raw_input("Which mode would you like to use? ")
+choice = raw_input("Which mode would you like to use? ").lower()
 
 
 cap = raw_input("How much exp do you need in total to level up? ")
 accumulated = raw_input("How much exp have you accumulated so far? ")
-expneeded = int(cap) - int(accumulated)
-expneeded = rtnh(expneeded)
+expneeded = rtnh(int(cap) - int(accumulated))
 
 
 #Variables for choice 1 (Basic/Got all the candy)
-pktoevolve = rtnh(expneeded) // 500
+pktoevolve = rtnh(expneeded) // EXP_FROM_EVOLUTION
 pktocatch = rtnh(expneeded) / 100
 
-lol = rtnh(expneeded) % 500
+lol = rtnh(expneeded) % EXP_FROM_EVOLUTION
 pktosituational = lol / 100
 
-#Variables for choice 2 (Smart/Catching one type of pokemon)
 
 
 if int(cap) < int(accumulated):
     print "Sorry, you could not have accumulated %s exp if you needed %s exp in total." % (accumulated, cap)
-elif int(cap) >= int(accumulated):
-    if choice == "Basic" or choice == "Got all the candy" or choice == "Basic/Got all the candy" or choice == "1. Basic/Got all the candy" or choice == "1":
-        if rtnh(expneeded) % 500 == 0:
+else:
+    if choice == "basic" or choice == "got all the candy" or choice == "basic/got all the candy" or choice == "1. basic/got all the candy" or choice == "1":
+        if rtnh(expneeded) % EXP_FROM_EVOLUTION == 0:
             print "Ok, so you\'ll need to catch %s more pokemon or evolve/discover %s more pokemon." % (pktocatch, pktoevolve)
-        elif rtnh(expneeded) % 500 != 0 and pktoevolve != 0:
+        elif rtnh(expneeded) % EXP_FROM_EVOLUTION != 0 and pktoevolve != 0:
             print "Ok, so you\'ll need to catch %s more pokemon or catch %s more pokemon and evolve/discover %s pokemon." % (pktocatch, pktosituational, pktoevolve)
         elif pktoevolve < 0:
             print "Ok, so you\'ll need to catch %s more pokemon." % (pktocatch)
@@ -50,17 +51,16 @@ elif int(cap) >= int(accumulated):
             print "Sorry, that is not a valid choice!"
     
     
-    elif choice == "Smart" or choice == "Catching and evolving one Pokemon" or choice == "Smart/Catching and Evolving one Pokemon" or choice == "2. Smart/Catching and Evolving one Pokemon" or choice == "2":
+    elif choice == "smart" or choice == "catching and evolving one pokemon" or choice == "smart/catching and evolving one pokemon" or choice == "2. smart/catching and evolving one pokemon" or choice == "2":
         candiesneeded = raw_input("How many candies do you need per evolution? ")
-        zed = int(candiesneeded)
         
-        z_over_four = zed//4 + zed % 4
-        exp_from_z_over_four = z_over_four * 100 + 500
+        zed = int(candiesneeded)
+        z_over_four = zed//CANDY_PER_PK + zed % CANDY_PER_PK
+        exp_from_z_over_four = z_over_four * 100 + EXP_FROM_EVOLUTION
         
         
         pktoevolve_smart = expneeded // exp_from_z_over_four
         extrapktocatch = (expneeded % exp_from_z_over_four)/100
-        
         
         pktocatch_smart = pktoevolve_smart * z_over_four + extrapktocatch
         
